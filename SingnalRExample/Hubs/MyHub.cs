9faee8906efea7fa73b0ea8b91c0deaf05294683
Muhammed Zayif <muhammed.zayif@gmail.com>
2024-03-31 02:49:55 +0300

    Add project files.

---
+++ SingnalRExample/Hubs/MyHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRExample.Hubs;
+
+public class MyHub: Hub
+{
+    public async Task SendMessageAsync(string message)
+    {
+        await Clients.All.SendAsync("receiveMessage", message);
+    }
+}
